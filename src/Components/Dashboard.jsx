74edd006abef3e032566b3883601c24a3be9cfd4
Codewithtechfrenz <techfrenz2025@gmail.com>
2026-04-02 00:26:31 +0530

    updated code

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,8 +1,37 @@
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import Sidebar from "./Sidebar";
+// import Topbar from "./Topbar";
+// import Page from "./Page";
+
+// function Dashboard() {
+//   return (
+//     <div style={{ display: "flex" }}>
+//       <Sidebar />
+
+//       <div className="main-area">
+//         <Topbar />
+
+//         <div className="page-content">
+//           <Routes>
+//             <Route path="/" element={<Page />} />
+//           </Routes>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Dashboard;
+
+
+
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 import Page from "./Page";
+import InvoiceTemplate from "./InvoiceTemplate"; // add this
 
 function Dashboard() {
   return (
@@ -15,6 +44,9 @@ function Dashboard() {
         <div className="page-content">
           <Routes>
             <Route path="/" element={<Page />} />
+
+            {/* ADD THIS */}
+            <Route path="invoice/:invoiceNo" element={<InvoiceTemplate />} />
           </Routes>
         </div>
       </div>
